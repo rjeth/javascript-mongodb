07dd81858af0029518f8d6321a62ae5a9bb83dd5
ジェスロ・ジョロラン <40770876+rjeth@users.noreply.github.com>
2021-11-27 01:15:40 +0800

    mongodb driver

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express'); 
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient; 
 const app = express();
 
 
+
+
+
 app.get("/" ,(req, res) =>{
-    res.send("Hello World");
-})
+    res.json({ message: "Hello" });
+});
+
+app.get("/api/airbnb" , async ( req , res) =>{
+    // const client = await MongoClient.connect("mongodb://localhost:27017", { 
+    //     useNewUrlParser: true,
+    // });
+
+    // const db = client.db("sample_airbnb");
+    // const airbnb = await db.collection('listingsAndReviews').find({}).toArray();
+
+    // res.json({ airbnb });
+    res.json({ message: "Hello" });
+});
 
 app.listen(1337);
